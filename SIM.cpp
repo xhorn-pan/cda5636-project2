0781d55ded997a7b9a9112f38a864aa5a33edd86
Xinghua Pan <xi.pan@ufl.edu>
2020-04-02 11:35:08 -0400

    get rid of assert

--- SIM.cpp
+++ SIM.cpp
@@ -1,9 +1,11 @@
 /* On my honor, I have neither given nor received unauthorized aid on this
- * assignment */
+ * assignment
+ * Name: Xinghua Pan
+ * UFID: 95160902
+ */
 
 #include <algorithm>
 #include <bitset>
-#include <cassert>
 #include <fstream>
 #include <iomanip>
 #include <iostream>
@@ -11,22 +13,26 @@
 #include <string>
 #include <vector>
 
-struct seq_fre {
+struct seq_fre
+{
   unsigned long code;
   int seq, fre;
-  friend bool operator<(const seq_fre &lhs, const seq_fre &rhs) {
+  friend bool operator<(const seq_fre& lhs, const seq_fre& rhs)
+  {
     if (lhs.fre == rhs.fre) {
       return lhs.seq > lhs.seq;
     } else {
       return lhs.fre < rhs.fre;
     }
   };
-  friend bool operator==(const seq_fre &lhs, const seq_fre &rhs) {
+  friend bool operator==(const seq_fre& lhs, const seq_fre& rhs)
+  {
     return lhs.code == rhs.code;
   };
 };
 
-enum fmt_type : unsigned long long {
+enum fmt_type : unsigned long long
+{
   RLE,
   BITMASK,
   ONE_MM,
@@ -36,55 +42,61 @@ enum fmt_type : unsigned long long {
   ORIG,
   FIN
 };
-struct compress_code {
+struct compress_code
+{
   fmt_type fmt;
   unsigned long long cc;
   std::size_t size;
-  friend bool operator<(const compress_code &lhs, const compress_code &rhs) {
+  friend bool operator<(const compress_code& lhs, const compress_code& rhs)
+  {
     if (lhs.size == rhs.size) {
       return lhs.fmt > rhs.fmt;
     }
     return lhs.size < rhs.size;
   };
-  friend std::ostream &operator<<(std::ostream &os, const compress_code &cc) {
+  friend std::ostream& operator<<(std::ostream& os, const compress_code& cc)
+  {
     switch (cc.fmt) {
-    case fmt_type::RLE: {
-      os << std::bitset<5>(cc.cc);
-      break;
-    }
-    case fmt_type::BITMASK: {
-      os << std::bitset<15>(cc.cc);
-      break;
-    }
-    case fmt_type::ONE_MM: {
-      os << std::bitset<11>(cc.cc);
-      break;
-    }
-    case fmt_type::TWO_MM: {
-      os << std::bitset<16>(cc.cc);
-      break;
-    }
-    case fmt_type::TWO_CMM: {
-      os << std::bitset<11>(cc.cc);
-      break;
-    }
-    case fmt_type::DM: {
-      os << std::bitset<6>(cc.cc);
-      break;
-    }
-    case fmt_type::ORIG: {
-      os << std::bitset<35>(cc.cc);
-      break;
-    }
-    default:
-      break;
+      case fmt_type::RLE: {
+        os << std::bitset<5>(cc.cc);
+        break;
+      }
+      case fmt_type::BITMASK: {
+        os << std::bitset<15>(cc.cc);
+        break;
+      }
+      case fmt_type::ONE_MM: {
+        os << std::bitset<11>(cc.cc);
+        break;
+      }
+      case fmt_type::TWO_MM: {
+        os << std::bitset<16>(cc.cc);
+        break;
+      }
+      case fmt_type::TWO_CMM: {
+        os << std::bitset<11>(cc.cc);
+        break;
+      }
+      case fmt_type::DM: {
+        os << std::bitset<6>(cc.cc);
+        break;
+      }
+      case fmt_type::ORIG: {
+        os << std::bitset<35>(cc.cc);
+        break;
+      }
+      default:
+        break;
     }
     return os;
   }
 };
 
-void read_compressed(std::string in_file, std::string *compressed,
-                     std::vector<std::string> *dicts) {
+void
+read_compressed(std::string in_file,
+                std::string* compressed,
+                std::vector<std::string>* dicts)
+{
   std::ifstream file(in_file);
   std::string line;
   bool rd = false;
@@ -105,20 +117,25 @@ void read_compressed(std::string in_file, std::string *compressed,
   }
 };
 
-void read_uncompressed(std::string in_file, std::vector<std::string> *codes,
-                       std::vector<std::string> *dicts) {
+void
+read_uncompressed(std::string in_file,
+                  std::vector<std::string>* codes,
+                  std::vector<std::string>* dicts)
+{
 
   std::ifstream file(in_file);
   std::string line;
   int seq = 0;
   std::vector<seq_fre> vs;
   std::vector<seq_fre>::iterator it;
-  while (getline(file, line)) {
-    // std::cout << "line " << ++seq << ": \t" << line << std::endl;
-    assert(line.size() == 32);
+  while (!file.eof()) {
+    getline(file, line);
+    if (line[line.length() - 1] == '\r') { // check crlf
+      line = line.substr(0, line.length() - 1);
+    }
     codes->push_back(line);
     unsigned long bu = std::bitset<32>(line).to_ulong();
-    seq_fre sf_{bu, seq, 1};
+    seq_fre sf_{ bu, seq, 1 };
     it = find(vs.begin(), vs.end(), sf_);
     if (it != vs.end()) {
       it->fre += 1;
@@ -136,7 +153,9 @@ void read_uncompressed(std::string in_file, std::vector<std::string> *codes,
   }
 }
 
-void decompress(std::string *code, std::vector<std::string> *dicts) {
+void
+decompress(std::string* code, std::vector<std::string>* dicts)
+{
 
   std::istringstream iss(*code);
   std::string prev;
@@ -148,108 +167,110 @@ void decompress(std::string *code, std::vector<std::string> *dicts) {
     fmt_type ft = static_cast<fmt_type>(std::bitset<3>(fmt_t).to_ullong());
 
     switch (ft) {
-    case fmt_type::RLE: {
-      char rep[3];
-      iss.get(rep, 3);
-      auto rep_i = std::bitset<2>(rep).to_ullong();
-      for (std::size_t i = 0; i <= rep_i; ++i) {
-        std::cout << prev << std::endl;
+      case fmt_type::RLE: {
+        char rep[3];
+        iss.get(rep, 3);
+        auto rep_i = std::bitset<2>(rep).to_ullong();
+        for (std::size_t i = 0; i <= rep_i; ++i) {
+          std::cout << prev << std::endl;
+        }
+        break;
       }
-      break;
-    }
-    case fmt_type::BITMASK: {
-      char sloc[6], bitmask[5], di[4];
-      iss.get(sloc, 6);
-      iss.get(bitmask, 5);
-      iss.get(di, 4);
-      auto dii = std::bitset<5>(di).to_ullong();
-      auto d = dicts->at(dii);
-      auto ulld = std::bitset<64>(d).to_ullong();
-      auto loc_i = std::bitset<5>(sloc).to_ullong();
-      auto bm = std::bitset<5>(bitmask).to_ullong();
-      unsigned long long diff = bm << (28 - loc_i);
+      case fmt_type::BITMASK: {
+        char sloc[6], bitmask[5], di[4];
+        iss.get(sloc, 6);
+        iss.get(bitmask, 5);
+        iss.get(di, 4);
+        auto dii = std::bitset<5>(di).to_ullong();
+        auto d = dicts->at(dii);
+        auto ulld = std::bitset<64>(d).to_ullong();
+        auto loc_i = std::bitset<5>(sloc).to_ullong();
+        auto bm = std::bitset<5>(bitmask).to_ullong();
+        unsigned long long diff = bm << (28 - loc_i);
 
-      auto dc = ulld ^ diff;
-      prev = std::bitset<32>(dc).to_string();
-      std::cout << prev << std::endl;
-      break;
-    }
-    case fmt_type::ONE_MM: {
-      char mloc[6], di[4];
-      iss.get(mloc, 6);
-      iss.get(di, 4);
-      auto loc_i = std::bitset<5>(mloc).to_ullong();
-      auto dii = std::bitset<5>(di).to_ullong();
-      unsigned long long diff = 1 << (31 - loc_i);
-      auto d = dicts->at(dii);
-      auto ulld = std::bitset<64>(d).to_ullong();
-      auto dc = ulld ^ diff;
-      prev = std::bitset<32>(dc).to_string();
-      std::cout << prev << std::endl;
+        auto dc = ulld ^ diff;
+        prev = std::bitset<32>(dc).to_string();
+        std::cout << prev << std::endl;
+        break;
+      }
+      case fmt_type::ONE_MM: {
+        char mloc[6], di[4];
+        iss.get(mloc, 6);
+        iss.get(di, 4);
+        auto loc_i = std::bitset<5>(mloc).to_ullong();
+        auto dii = std::bitset<5>(di).to_ullong();
+        unsigned long long diff = 1 << (31 - loc_i);
+        auto d = dicts->at(dii);
+        auto ulld = std::bitset<64>(d).to_ullong();
+        auto dc = ulld ^ diff;
+        prev = std::bitset<32>(dc).to_string();
+        std::cout << prev << std::endl;
 
-      break;
-    }
-    case fmt_type::TWO_MM: {
-      char mloc1[6], mloc2[6], di[4];
-      iss.get(mloc1, 6);
-      iss.get(mloc2, 6);
-      iss.get(di, 4);
-      auto loc_i1 = std::bitset<5>(mloc1).to_ullong();
-      auto loc_i2 = std::bitset<5>(mloc2).to_ullong();
-      auto dii = std::bitset<5>(di).to_ullong();
-      auto d = dicts->at(dii);
-      auto ulld = std::bitset<64>(d).to_ullong();
+        break;
+      }
+      case fmt_type::TWO_MM: {
+        char mloc1[6], mloc2[6], di[4];
+        iss.get(mloc1, 6);
+        iss.get(mloc2, 6);
+        iss.get(di, 4);
+        auto loc_i1 = std::bitset<5>(mloc1).to_ullong();
+        auto loc_i2 = std::bitset<5>(mloc2).to_ullong();
+        auto dii = std::bitset<5>(di).to_ullong();
+        auto d = dicts->at(dii);
+        auto ulld = std::bitset<64>(d).to_ullong();
 
-      unsigned long long diff1 = 1 << (31 - loc_i1);
-      unsigned long long diff2 = 1 << (31 - loc_i2);
+        unsigned long long diff1 = 1 << (31 - loc_i1);
+        unsigned long long diff2 = 1 << (31 - loc_i2);
 
-      auto dc = ulld ^ diff1 ^ diff2;
-      prev = std::bitset<32>(dc).to_string();
-      std::cout << prev << std::endl;
+        auto dc = ulld ^ diff1 ^ diff2;
+        prev = std::bitset<32>(dc).to_string();
+        std::cout << prev << std::endl;
 
-      break;
-    }
-    case fmt_type::TWO_CMM: {
-      char mloc[6], di[4];
-      iss.get(mloc, 6);
-      iss.get(di, 4);
-      auto loc_i = std::bitset<5>(mloc).to_ullong();
-      auto dii = std::bitset<5>(di).to_ullong();
-      unsigned long long diff = 3 << (30 - loc_i);
-      auto d = dicts->at(dii);
-      auto ulld = std::bitset<64>(d).to_ullong();
-      auto dc = ulld ^ diff;
-      prev = std::bitset<32>(dc).to_string();
-      std::cout << prev << std::endl;
-      break;
-    }
-    case fmt_type::DM: {
-      char di[4];
-      iss.get(di, 4);
-      auto dii = std::bitset<5>(di).to_ullong();
-      prev = dicts->at(dii);
-      std::cout << prev << std::endl;
-      break;
-    }
-    case fmt_type::ORIG: {
-      char orig_c[33];
-      iss.get(orig_c, 33);
-      prev = std::string(orig_c);
-      std::cout << prev << std::endl;
-      break;
-    }
-    case fmt_type::FIN: {
-      fin = true;
-      break;
-    }
-    default:
-      fin = true;
-      break;
+        break;
+      }
+      case fmt_type::TWO_CMM: {
+        char mloc[6], di[4];
+        iss.get(mloc, 6);
+        iss.get(di, 4);
+        auto loc_i = std::bitset<5>(mloc).to_ullong();
+        auto dii = std::bitset<5>(di).to_ullong();
+        unsigned long long diff = 3 << (30 - loc_i);
+        auto d = dicts->at(dii);
+        auto ulld = std::bitset<64>(d).to_ullong();
+        auto dc = ulld ^ diff;
+        prev = std::bitset<32>(dc).to_string();
+        std::cout << prev << std::endl;
+        break;
+      }
+      case fmt_type::DM: {
+        char di[4];
+        iss.get(di, 4);
+        auto dii = std::bitset<5>(di).to_ullong();
+        prev = dicts->at(dii);
+        std::cout << prev << std::endl;
+        break;
+      }
+      case fmt_type::ORIG: {
+        char orig_c[33];
+        iss.get(orig_c, 33);
+        prev = std::string(orig_c);
+        std::cout << prev << std::endl;
+        break;
+      }
+      case fmt_type::FIN: {
+        fin = true;
+        break;
+      }
+      default:
+        fin = true;
+        break;
     }
   }
 }
 
-std::string compress1(std::string *code, std::vector<std::string> *dicts) {
+std::string
+compress1(std::string* code, std::vector<std::string>* dicts)
+{
   std::vector<compress_code> ccs;
   for (std::size_t dict_idx = 0; dict_idx != dicts->size(); ++dict_idx) {
     auto code_l = std::bitset<64>(*code).to_ulong();
@@ -264,17 +285,17 @@ std::string compress1(std::string *code, std::vector<std::string> *dicts) {
     auto ds = diff_ones.size();
     if (ds > 4) { // 110 continues to next dict
       unsigned long long cc = (fmt_type::ORIG << 32) ^ code_l;
-      ccs.push_back(compress_code{fmt_type::ORIG, cc, 35});
+      ccs.push_back(compress_code{ fmt_type::ORIG, cc, 35 });
     } else if (ds >= 3) {
       auto distance = diff_ones[ds - 1] - diff_ones[0];
       if (distance > 4) { // 110 continues to next dict
         unsigned long long cc = (fmt_type::ORIG << 32) ^ code_l;
-        ccs.push_back(compress_code{fmt_type::ORIG, cc, 35});
+        ccs.push_back(compress_code{ fmt_type::ORIG, cc, 35 });
       } else { // 001 , get the bitmask
         unsigned long bm = diff >> (32 - 4 - diff_ones[0]);
         unsigned long cc = (fmt_type::BITMASK << (5 + 4 + 3)) ^
                            (diff_ones[0] << (4 + 3)) ^ (bm << 3) ^ dict_idx;
-        ccs.push_back(compress_code{fmt_type::BITMASK, cc, 15});
+        ccs.push_back(compress_code{ fmt_type::BITMASK, cc, 15 });
       }
     } else if (ds == 2) {
       auto distance = diff_ones[1] - diff_ones[0];
@@ -282,24 +303,24 @@ std::string compress1(std::string *code, std::vector<std::string> *dicts) {
         unsigned long cc = (fmt_type::TWO_MM << (5 + 5 + 3)) ^
                            (diff_ones[0] << (5 + 3)) ^ (diff_ones[1] << 3) ^
                            dict_idx;
-        ccs.push_back(compress_code{fmt_type::TWO_MM, cc, 16});
+        ccs.push_back(compress_code{ fmt_type::TWO_MM, cc, 16 });
       } else if (distance > 1) { // 001 get bitmask
         unsigned long bm = diff >> (32 - 4 - diff_ones[0]);
         unsigned long cc = (fmt_type::BITMASK << (5 + 4 + 3)) ^
                            (diff_ones[0] << (4 + 3)) ^ (bm << 3) ^ dict_idx;
-        ccs.push_back(compress_code{fmt_type::BITMASK, cc, 15});
+        ccs.push_back(compress_code{ fmt_type::BITMASK, cc, 15 });
       } else { // distance==1, 011
         unsigned long cc =
-            (fmt_type::TWO_CMM << (5 + 3)) ^ (diff_ones[0] << 3) ^ dict_idx;
-        ccs.push_back(compress_code{fmt_type::TWO_CMM, cc, 11});
+          (fmt_type::TWO_CMM << (5 + 3)) ^ (diff_ones[0] << 3) ^ dict_idx;
+        ccs.push_back(compress_code{ fmt_type::TWO_CMM, cc, 11 });
       }
     } else if (ds == 1) { // 010
       unsigned long cc =
-          (fmt_type::ONE_MM << (5 + 3)) ^ (diff_ones[0] << 3) ^ dict_idx;
-      ccs.push_back(compress_code{fmt_type::ONE_MM, cc, 11});
+        (fmt_type::ONE_MM << (5 + 3)) ^ (diff_ones[0] << 3) ^ dict_idx;
+      ccs.push_back(compress_code{ fmt_type::ONE_MM, cc, 11 });
     } else { // ds = 0, 101
       unsigned long cc = (fmt_type::DM << 3) ^ dict_idx;
-      ccs.push_back(compress_code{fmt_type::DM, cc, 6});
+      ccs.push_back(compress_code{ fmt_type::DM, cc, 6 });
     }
   } // end for each dict
   std::sort(ccs.begin(), ccs.end());
@@ -308,16 +329,17 @@ std::string compress1(std::string *code, std::vector<std::string> *dicts) {
   return ss.str();
 }
 
-std::string compress(std::vector<std::string> *codes,
-                     std::vector<std::string> *dicts) {
+std::string
+compress(std::vector<std::string>* codes, std::vector<std::string>* dicts)
+{
   std::stringstream ss;
   std::string prev = "";
   int rep = -1;
-  //std::cerr << "Code:" << std::endl;
+  // std::cerr << "Code:" << std::endl;
   for (std::size_t ci = 0; ci != codes->size(); ++ci) {
     if (codes->at(ci) == prev) {
       if (rep == 3) {
-        compress_code pc{fmt_type::RLE, (unsigned long)rep, 5};
+        compress_code pc{ fmt_type::RLE, (unsigned long)rep, 5 };
         ss << pc;
         rep = -1;
       } else {
@@ -326,7 +348,7 @@ std::string compress(std::vector<std::string> *codes,
       continue;
     } else {
       if (rep >= 0) {
-        compress_code pc{fmt_type::RLE, (unsigned long)rep, 5};
+        compress_code pc{ fmt_type::RLE, (unsigned long)rep, 5 };
         ss << pc;
         rep = -1;
       } // else {
@@ -338,7 +360,9 @@ std::string compress(std::vector<std::string> *codes,
   return ss.str(); //
 }
 
-int main(int argc, char *argv[]) {
+int
+main(int argc, char* argv[])
+{
   if (argc != 2) {
     std::cerr << "Usage: \t" << argv[0] << " 1 : compression" << std::endl;
     std::cerr << "\t" << argv[0] << " 2 : decompression" << std::endl;
